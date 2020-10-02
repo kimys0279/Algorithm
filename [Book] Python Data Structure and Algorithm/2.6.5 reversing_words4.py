340d9cb447f1cc842d17bf7f2014a98095c2fca8
kimys0279 <68838464+kimys0279@users.noreply.github.com>
2020-10-02 13:16:05 +0900

    Create 2.6.5 reversing_words4.py

---
+++ [Book] Python Data Structure and Algorithm/2.6.5 reversing_words4.py
@@ -0,0 +1,16 @@
+def reversing_words_slice(str1):
+  words = str1.split(" ")
+  rev_set = " ".join(reversed(words))
+  return rev_set
+
+def reversing_words2(str1):
+  words = str1.split(" ")
+  words.reverse()
+  return " ".join(words)
+
+if __name__ == "__main__":
+  str1 = "파이썬 알고리즘 정말 재미있다"
+  str2 = reversing_words(str1)
+  str3 = reversing_words2(str1)
+  print(str2)
+  print(str3)
