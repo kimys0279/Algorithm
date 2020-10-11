07d193e91197771dc701ea0410b2ec5b67277701
kimys0279 <68838464+kimys0279@users.noreply.github.com>
2020-10-11 17:32:17 +0900

    Create [S3] 2559 수열.py

---
+++ [BOJ] 백준/[S3] 2559 수열.py
@@ -0,0 +1,32 @@
+import sys
+def solve(n,k,t):
+    temp = sum(t[:k])
+    ans = temp
+    for i in range(k,n):
+        temp += t[i]
+        temp -= t[i-k]
+        if temp > ans:
+            ans = temp
+    return ans if n != k else temp
+if __name__ == "__main__":
+    n, k = map(int,sys.stdin.readline().split())
+    t = list(map(int,sys.stdin.readline().split()))
+    print(solve(n,k,t))
+    
+
+<오답 - 런타임에러>
+import sys
+def solve(n, k, t):
+    temp = sum(t[:k])
+    ans = temp
+    for i in range(k, n):
+        temp += t[i]
+        temp -= t[i-k]
+        if temp > ans:
+            ans = temp
+    return ans if n != k else temp
+
+if __name__ == "__main__":
+    n, k = map(int, sys.stdin.readline().split())
+    t = list(map(int, sts.stdin.readline().split()))
+    print(solve(n, k, t))
