75592e03534fffbd5d113ee76253fbe391fa0cc0
Yuseung Kim <ykim49@ncsu.edu>
2021-01-14 15:43:52 -0500

    Update greedy 1.py

--- [Book] 이것이 취업을 위한 코딩 테스트다/greedy 1.py
+++ [Book] 이것이 취업을 위한 코딩 테스트다/greedy 1.py
@@ -9,3 +9,26 @@ for coin in array:
     n %= coin
     
 print(count)
+
+
+
+''
+C++
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n = 1260;
+int cnt;
+
+int coinTypes[4] = {500, 100, 50, 10};
+
+int main(void) {
+    for (int i = 0; i < 4; i++) {
+        cnt += n / coinTypes[i];
+        n %= coinTypes[i];    
+    }
+    cout << cnt << '\n';
+}
+''
