858942507ab4541d3f63d1c8150aa869c8dcc2da
Yuseung Kim <ykim49@ncsu.edu>
2021-02-11 19:09:49 -0500

    Create [B1] 2750 수 정렬하기.py

---
+++ [BOJ] 백준/[B1] 2750 수 정렬하기.py
@@ -0,0 +1,39 @@
+'''
+첫째 줄에 수의 개수 N(1 ≤ N ≤ 1,000)이 주어진다. 둘째 줄부터 N개의 줄에는 숫자가 주어진다. 이 수는 절댓값이 1,000보다 작거나 같은 정수이다. 수는 중복되지 않는다.
+
+출력
+첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
+
+예제 입력 1 
+5
+5
+2
+3
+4
+1
+예제 출력 1 
+1
+2
+3
+4
+5
+'''
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+a = []
+for _ in range(N):
+    i = int(input())
+    a.append(i)
+
+a.sort()
+
+for j in range(N):
+    print(a[j])
+
+
+
+
+
