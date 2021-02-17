cab795be0f4a97beff627e59930ec8f2cb173b71
Yuseung Kim <ykim49@ncsu.edu>
2021-02-17 09:57:46 -0500

    Create [B1] 1157 단어 공부.py

---
+++ [BOJ] 백준/[B1] 1157 단어 공부.py
@@ -0,0 +1,36 @@
+'''
+알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성하시오. 단, 대문자와 소문자를 구분하지 않는다.
+
+입력
+첫째 줄에 알파벳 대소문자로 이루어진 단어가 주어진다. 주어지는 단어의 길이는 1,000,000을 넘지 않는다.
+
+출력
+첫째 줄에 이 단어에서 가장 많이 사용된 알파벳을 대문자로 출력한다. 단, 가장 많이 사용된 알파벳이 여러 개 존재하는 경우에는 ?를 출력한다.
+
+예제 입력 1 
+Mississipi
+예제 출력 1 
+?
+예제 입력 2 
+zZa
+예제 출력 2 
+Z
+예제 입력 3 
+z
+예제 출력 3 
+Z'''
+
+
+words = input().upper()
+unique_words = list(set(words))  # 입력받은 문자열에서 중복값을 제거
+
+cnt_list = []
+for x in unique_words :
+    cnt = words.count(x)
+    cnt_list.append(cnt)  # count 숫자를 리스트에 append
+
+if cnt_list.count(max(cnt_list)) > 1 :  # count 숫자 최대값이 중복되면
+    print('?')
+else :
+    max_index = cnt_list.index(max(cnt_list))  # count 숫자 최대값 인덱스(위치)
+    print(unique_words[max_index])
