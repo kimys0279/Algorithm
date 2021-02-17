bcbca58dd61a360bf04f98bf2442d5300b9aafb9
Yuseung Kim <ykim49@ncsu.edu>
2021-02-17 12:48:06 -0500

    Create [S2] 1929 소수 구하기.py

---
+++ [BOJ] 백준/[S2] 1929 소수 구하기.py
@@ -0,0 +1,33 @@
+'''
+첫째 줄에 자연수 M과 N이 빈 칸을 사이에 두고 주어진다. (1 ≤ M ≤ N ≤ 1,000,000) 
+M이상 N이하의 소수가 하나 이상 있는 입력만 주어진다.
+
+출력
+한 줄에 하나씩, 증가하는 순서대로 소수를 출력한다.
+
+예제 입력 1 
+3 16
+예제 출력 1 
+3
+5
+7
+11
+13'''
+
+def sosu(num):
+    if num == 1:
+        return False
+    else:
+        for i in range(2, int(num**0.5)+1):
+            if num % i == 0:
+                return False
+        return True
+
+import sys
+input = sys.stdin.readline
+
+m, n = map(int, input().split())
+
+for i in range(m, n+1):
+    if sosu(i):
+        print(i)
