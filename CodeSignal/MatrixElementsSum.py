cd8be16efac2f06e40a31df7d88c3146a98cb534
Yuseung Kim <ykim49@ncsu.edu>
2021-10-31 12:00:13 -0400

    Update MatrixElementsSum.py

--- CodeSignal/MatrixElementsSum.py
+++ CodeSignal/MatrixElementsSum.py
@@ -9,3 +9,14 @@ def matrixElementsSum(m):
             else:
                 break
     return total
+
+def matrixElementsSum(matrix):
+
+    sum = 0
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] == 0 and i+1 < len(matrix):
+                matrix[i+1][j] = 0
+            sum += matrix[i][j]
+    
+    return sum
