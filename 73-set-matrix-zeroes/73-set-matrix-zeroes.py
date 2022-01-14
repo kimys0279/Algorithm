e3b195cdc47d7e7a234b6153a09c85258830b21a
Yuseung Kim <ykim49@ncsu.edu>
2022-01-14 17:48:18 -0500

    Time: 155 ms (27.14%), Space: 14.5 MB (23.68%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        m = len(matrix)
+        n = len(matrix[0])
+        
+        if not matrix:
+            return []
+        
+        zeroes_row = [False] * m
+        zeroes_col = [False] * n
+        
+        for row in range(m):
+            for col in range(n):
+                if matrix[row][col] == 0:
+                    zeroes_row[row] = True
+                    zeroes_col[col] = True
+                    
+        
+        for row in range(m):
+            for col in range(n):
+                if zeroes_row[row] or zeroes_col[col]:
+                    matrix[row][col] = 0
+        
