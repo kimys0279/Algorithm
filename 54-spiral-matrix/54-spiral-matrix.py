8d10e6b1adadf0f8e31aec10e5f4067ed0745351
Yuseung Kim <ykim49@ncsu.edu>
2022-01-14 19:46:13 -0500

    Time: 18 ms (59.48%), Space: 13.5 MB (43.70%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -4,21 +4,39 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: List[int]
         """
-        result = []
+        left, right = 0, len(matrix[0]) - 1
+        top, bottom = 0, len(matrix) - 1
         
-        while matrix and matrix[0]:
-            if matrix[0]:
-                result += matrix.pop(0)
+        ans = []
+        
+        while True:
+            #top go right
+            ans.extend(matrix[top][x] for x in range(left, right + 1))
+            top += 1
                 
-            if matrix and matrix[0]:
-                for row in matrix:
-                    result.append(row.pop())
-                    
-            if matrix and matrix[-1]:
-                result += matrix.pop()[::-1]
+            if top > bottom:
+                break
+            
+            #Right side go down
+            ans.extend(matrix[y][right] for y in range(top, bottom + 1))
+            right -= 1
                 
-            if matrix and matrix[0]:
-                for row in matrix[::-1]:
-                    result.append(row.pop(0))
+            if left > right:
+                break
+            
+            #Bottom side go left
+            ans.extend(matrix[bottom][x] for x in range(right, left - 1, -1))
+            bottom -= 1
+                
+            if top > bottom:
+                break
+            
+            #Left side go up
+            ans.extend(matrix[y][left] for y in range(bottom, top - 1, -1))
+            left += 1
+                
+            if left > right:
+                break
                     
-        return result
+        return ans
+            
