452d3ee62252048bfca616f75146f34022abf74a
Yuseung Kim <ykim49@ncsu.edu>
2022-01-18 12:35:45 -0500

    Time: 53 ms (5.22%), Space: 13.6 MB (10.34%) - LeetHub

---
+++ 190-reverse-bits/190-reverse-bits.py
@@ -0,0 +1,9 @@
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        res = 0
+        for _ in range(32):
+            res = res*2 + (n & 1)
+            n = n >> 1
+        return res
