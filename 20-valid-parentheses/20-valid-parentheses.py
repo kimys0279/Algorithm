5095302f191196b6ea8ab83ad799ec1f95432ad4
Yuseung Kim <ykim49@ncsu.edu>
2022-02-26 11:32:59 -0500

    Time: 22 ms (72.22%), Space: 13.5 MB (83.82%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -14,3 +14,20 @@ class Solution(object):
                 return False
             
         return len(stack) == 0
+    
+    
+    def isValid(self, s):
+        
+        dic = {'(':1, ')':2, '{':3, '}':4, '[':5, ']':6}
+        ans = []
+        
+        for one in s:
+            if (dic[one] % 2 == 1):
+                ans.append(dic[one])
+            else:
+                if(len(ans) and ans[-1] == dic[one] - 1):
+                    del ans[-1]
+                else:
+                    return False
+                
+        return ans == []
