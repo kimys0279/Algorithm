66a64ff0afc438de61d541a3b441fb428dc3becd
Yuseung Kim <ykim49@ncsu.edu>
2022-02-27 16:29:37 -0500

    Time: 1661 ms (15.68%), Space: 15.4 MB (30.39%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,16 +1,5 @@
 class Solution(object):
-    def isPalindrome1(self, s):
-        
-        new = ''
-        s = s.lower()
-        
-        for i in s:
-            if i.isalnum():
-                new = new + i
-                
-        return new == new[::-1]
-    
-    
+
     def isPalindrome(self, s):
         
         if len(str(s)) == 0:
