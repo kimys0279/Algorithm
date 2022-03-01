4920fd712c2bb8978777f9985da623062451bc8f
Yuseung Kim <ykim49@ncsu.edu>
2022-03-01 12:31:19 -0500

    Time: 35 ms (91.70%), Space: 13.9 MB (69.55%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.py
@@ -1,25 +1,10 @@
 class Solution(object):
     def search(self, nums, target):
-        l, h = 0, len(nums) - 1
-        while l <= h:
-            mid = (l + h) // 2
-            if nums[mid] == target:
-                return True
-            
-            if nums[mid] == nums[l]:
-                l += 1
-            
-            elif nums[mid] > nums[h]:
-                if nums[l] <= target < nums[mid]:
-                    h = mid - 1
-                else:
-                    l = mid + 1
-            else:
-                if nums[mid] < target <= nums[h]:
-                    l = mid + 1
-                else:
-                    h = mid - 1
-        return False
+        if not nums:
+            return False
+        if target not in nums:
+            return False
+        return True
         
         
     
