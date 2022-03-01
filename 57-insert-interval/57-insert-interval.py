5a361efe58cd4ee0c2c54019ecbe9cb9bb44c97b
Yuseung Kim <ykim49@ncsu.edu>
2022-03-01 11:19:55 -0500

    Time: 112 ms (21.58%), Space: 16.6 MB (95.53%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -13,14 +13,16 @@ class Solution(object):
     
     
     
+    
+    
     def insert(self, intervals, newInterval):
         intervals.append(newInterval)
-        intervals.sort(key = lambda x:x[0])
+        intervals.sort(key = lambda x: x[0])
         ans = []
         
         for i in intervals:
-            if len(ans) == 0 or i[0] > ans[-1][1]:
+            if len(ans) == 0 or ans[-1][1] < i[0]:
                 ans.append(i)
-            elif i[0] <= ans[-1][1]:
+            elif ans[-1][1] >= i[0]:
                 ans[-1][1] = max(ans[-1][1], i[1])
         return ans
