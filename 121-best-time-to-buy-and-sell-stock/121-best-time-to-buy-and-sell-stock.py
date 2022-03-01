c984a037aa05e00d504cc0ebe8e805429325695c
Yuseung Kim <ykim49@ncsu.edu>
2022-03-01 13:16:24 -0500

    Time: 836 ms (94.30%), Space: 22.7 MB (33.33%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,22 +1,6 @@
 class Solution(object):
-    def maxProfit1(self, prices):
 
-        if len(prices) == 0:
-            return 0
-        
-        max = prices[-1]
-        profit = 0
-        
-        for item in prices[::-1]:
-            if max - item > profit:
-                profit = max - item
-            elif item > max:
-                max = item
-        
-        return profit
-    
-    
-    def maxProfit(self, prices):
+    def maxProfit1(self, prices):
         if len(prices) == 0:
             return 0
         
@@ -28,4 +12,19 @@ class Solution(object):
                 profit = max - item
             if item > max:
                 max = item
+        return profit
+    
+    
+    def maxProfit(self, prices):
+        if len(prices) == 0:
+            return 0
+        
+        sell = prices[-1]
+        profit = 0
+        
+        for buy in prices[::-1]:
+            if sell - buy > profit:
+                profit = sell - buy
+            if buy > sell:
+                sell = buy
         return profit
