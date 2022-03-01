214eada3e960d28da5c5bcadbcffafb1216a5411
Yuseung Kim <ykim49@ncsu.edu>
2022-03-01 09:03:35 -0500

    Time: 12 ms (98.94%), Space: 13.8 MB (13.36%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,19 +1,6 @@
 class Solution(object):
     
-    def isValid(self, s):
-        stack = []
-        dic = {"}":"{", ")":"(", "]":"["}
-        
-        for bra in s:
-            if bra in dic.values():
-                stack.append(bra)
-            elif bra in dic.keys():
-                if stack == [] or dic[bra] != stack.pop():
-                    return False
-            else:
-                return False
-        
-        return len(stack) == 0
+
     
     
     def isValid1(self, s):
