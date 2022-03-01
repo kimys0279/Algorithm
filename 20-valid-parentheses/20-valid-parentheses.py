5890bc0216e882ba3ce538cc6c07ad8663bf0773
Yuseung Kim <ykim49@ncsu.edu>
2022-03-01 16:41:08 -0500

    Time: 16 ms (94.44%), Space: 13.6 MB (60.52%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,5 +1,33 @@
 class Solution(object):
     
+    def isValid(self, s):
+        dic = {'(': 1, ')': 2, '{': 3, '}': 4, '[': 5, ']': 6}
+        ans = []
+        
+        for bra in s:
+            if (dic[bra] % 2 == 1):
+                ans.append(dic[bra])
+            else:
+                if (len(ans) != 0 and ans[-1] == dic[bra] - 1):
+                    del ans[-1]
+                else:
+                    return False
+        return len(ans) == 0
+    
+        
+    def isValid3(self, s):
+        dic = {'(': 1, ')': 2, '{': 3, '}': 4, '[': 5, ']': 6}
+        ans = []
+        
+        for bra in s:
+            if (dic[bra] % 2 == 1):
+                ans.append(dic[bra])
+            else:
+                if (len(ans) != 0 and ans[-1] == dic[bra]-1):
+                    del ans[-1]
+                else:
+                    return False
+        return ans == []
 
     
     
@@ -36,18 +64,3 @@ class Solution(object):
                 
         return ans == []
     
-    
-    
-    def isValid(self, s):
-        dic = {'(': 1, ')': 2, '{': 3, '}': 4, '[': 5, ']': 6}
-        ans = []
-        
-        for bra in s:
-            if (dic[bra] % 2 == 1):
-                ans.append(dic[bra])
-            else:
-                if (len(ans) != 0 and ans[-1] == dic[bra]-1):
-                    del ans[-1]
-                else:
-                    return False
-        return ans == []
