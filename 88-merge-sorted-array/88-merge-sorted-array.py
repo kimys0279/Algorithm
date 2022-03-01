8b319b1007e48635164f96656d44377903c18b14
Yuseung Kim <ykim49@ncsu.edu>
2022-03-01 12:44:31 -0500

    Time: 29 ms (62.36%), Space: 13.5 MB (18.77%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def merge(self, nums1, m, nums2, n):
+    def merge1(self, nums1, m, nums2, n):
         while m > 0 and n > 0:
             if nums1[m- 1] > nums2[n-1]:
                 nums1[m+n-1] = nums1[m-1]
@@ -11,4 +11,20 @@ class Solution(object):
         if n > 0:
             nums1[:n] = nums2[:n]
             
+        return nums1
+    
+    
+    
+    def merge(self, nums1, m, nums2, n):
+        while m > 0 and n > 0:
+            if nums1[m-1] > nums2[n-1]:
+                nums1[m+n-1] = nums1[m-1]
+                m -= 1
+            else:
+                nums1[m+n-1] = nums2[n-1]
+                n -= 1
+        
+        if n > 0:
+            nums1[:n] = nums2[:n]
+        
         return nums1
