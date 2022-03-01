47329659d188d00e4034314d1ea2b1b0cae84bb1
Yuseung Kim <ykim49@ncsu.edu>
2022-03-01 09:03:20 -0500

    Time: 35 ms (28.10%), Space: 13.8 MB (5.81%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,4 +1,21 @@
 class Solution(object):
+    
+    def isValid(self, s):
+        stack = []
+        dic = {"}":"{", ")":"(", "]":"["}
+        
+        for bra in s:
+            if bra in dic.values():
+                stack.append(bra)
+            elif bra in dic.keys():
+                if stack == [] or dic[bra] != stack.pop():
+                    return False
+            else:
+                return False
+        
+        return len(stack) == 0
+    
+    
     def isValid1(self, s):
         
         stack = []
@@ -16,7 +33,7 @@ class Solution(object):
         return len(stack) == 0
     
     
-    def isValid(self, s):
+    def isValid2(self, s):
         
         dic = {'(':1, ')':2, '{':3, '}':4, '[':5, ']':6}
         ans = []
@@ -30,4 +47,20 @@ class Solution(object):
                 else:
                     return False
                 
+        return ans == []
+    
+    
+    
+    def isValid(self, s):
+        dic = {'(': 1, ')': 2, '{': 3, '}': 4, '[': 5, ']': 6}
+        ans = []
+        
+        for bra in s:
+            if (dic[bra] % 2 == 1):
+                ans.append(dic[bra])
+            else:
+                if (len(ans) != 0 and ans[-1] == dic[bra]-1):
+                    del ans[-1]
+                else:
+                    return False
         return ans == []
