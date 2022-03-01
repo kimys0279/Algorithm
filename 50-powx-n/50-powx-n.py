4f76f58ea394fccd6394595e9e4e661ddcae1b8d
Yuseung Kim <ykim49@ncsu.edu>
2022-03-01 09:36:41 -0500

    Time: 31 ms (30.25%), Space: 13.4 MB (69.12%) - LeetHub

--- 50-powx-n/50-powx-n.py
+++ 50-powx-n/50-powx-n.py
@@ -1,8 +1,20 @@
 class Solution(object):
-    def myPow(self, x, n):
+    def myPow1(self, x, n):
         """
         :type x: float
         :type n: int
         :rtype: float
         """
         return x**n
+    
+    
+    def myPow(self, x, n):
+        if not n:
+            return 1
+        
+        if n < 0:
+            return 1 / self.myPow(x, -n)
+        
+        if n % 2:
+            return x * self.myPow(x, n - 1)
+        return self.myPow(x*x, n/2)
