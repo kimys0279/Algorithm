b5295f3612344c1810efdc7158d78c527bb2b0b5
Yuseung Kim <ykim49@ncsu.edu>
2022-03-01 11:16:17 -0500

    Time: 128 ms (76.98%), Space: 18.1 MB (71.52%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def merge(self, intervals):
+    def merge1(self, intervals):
         if len(intervals) == 0:
             return []
         intervals.sort(key = lambda x: x[0])
@@ -11,3 +11,27 @@ class Solution(object):
             elif (i[0] <= ans[-1][1]):
                 ans[-1][1] = max(ans[-1][1], i[1])
         return ans
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    def merge(self, intervals):
+        if len(intervals) == 0:
+            return []
+        
+        intervals.sort(key = lambda x: x[0])
+        ans = []
+        
+        for i in intervals:
+            if len(ans) == 0 or ans[-1][1] < i[0]:
+                ans.append(i)
+            elif ans[-1][1] >= i[0]:
+                ans[-1][1] = max(i[1], ans[-1][1])
+        return ans
+        
+        
