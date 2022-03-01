25ccd829c5095d9732e8aa82f43fc1ae7b8423a7
Yuseung Kim <ykim49@ncsu.edu>
2022-03-01 15:01:51 -0500

    Time: 40 ms (12.16%), Space: 13.5 MB (63.51%) - LeetHub

--- 166-fraction-to-recurring-decimal/166-fraction-to-recurring-decimal.py
+++ 166-fraction-to-recurring-decimal/166-fraction-to-recurring-decimal.py
@@ -33,3 +33,33 @@ class Solution(object):
             numerator %= denominator
             i += 1
         return res
+    
+    
+    def fractionToDecimal1(self, numerator, denominator):
+        res = ''
+        
+        if numerator / denominator < 0:
+            res += '-'
+            
+        if numerator % denominator == 0:
+            return str(numerator / denominator)
+        
+        numerator, denominator = abs(numerator), abs(denominator)
+        res += str(numerator/denominator) + '.'
+        
+        numerator %= denominator
+        i = len(res)
+        table = {}
+        while numerator != 0:
+            if numerator not in table.keys():
+                table[numerator] = i
+            else:
+                i = table[numerator]
+                res = res[:i] + '(' + res[i:] + ')'
+                return res
+            
+            numerator *= 10
+            res += str(numerator / denominator)
+            numerator %= denominator
+            i += 1
+        return res
