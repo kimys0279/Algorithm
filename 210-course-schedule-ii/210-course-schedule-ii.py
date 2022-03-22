d4956bc4fef98fec0010e66ac1e6e4f136d1c483
Yuseung Kim <ykim49@ncsu.edu>
2022-03-22 15:29:04 -0400

    Time: 124 ms (38.64%), Space: 14.7 MB (91.53%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -1,5 +1,34 @@
 class Solution(object):
     def findOrder(self, numCourses, prerequisites):
+        sortedorder = []
+        if numCourses <= 0:
+            return False
+        
+        inDegree = {i : 0 for i in range(numCourses)}
+        graph = {i : [] for i in range(numCourses)}
+        
+        for child, parent in prerequisites:
+            graph[parent].append(child)
+            inDegree[child] += 1
+            
+        sources = deque()
+        
+        for key in inDegree:
+            if inDegree[key] == 0:
+                sources.append(key)
+        while sources:
+            vertex = sources.popleft()
+            sortedorder.append(vertex)
+            for child in graph[vertex]:
+                inDegree[child] -= 1
+                if inDegree[child] == 0:
+                    sources.append(child)
+                    
+        if len(sortedorder) != numCourses:
+            return []
+        return sortedorder
+        
+    def findOrder1(self, numCourses, prerequisites):
         #dic[i] is the indegree of node i
         dic = [0 for i in range(numCourses)]
         neigh = collections.defaultdict(set)
