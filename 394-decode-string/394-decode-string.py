e55a8fe94f1d4623535e0ea86ead0ea50889f323
Yuseung Kim <ykim49@ncsu.edu>
2022-03-22 20:50:49 -0400

    Time: 28 ms (32.33%), Space: 13.6 MB (54.49%) - LeetHub

--- 394-decode-string/394-decode-string.py
+++ 394-decode-string/394-decode-string.py
@@ -1,27 +1,26 @@
 class Solution(object):
     def decodeString(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
         num = 0
         string = ''
-        stack = []
+        list = []
         
-        for c in s:
-            if c.isalnum():
-                if c.isdigit():
-                    num = num*10 + int(c)
-                elif c.isalpha():
-                    string += c
+        for i in s:
+            if i.isalpha(): #i > alphabet or number
+                string += i
+                
+            elif i.isdigit():
+                num = num*10 + int(i)
             
-            elif c == '[':
-                stack.append(string)
-                stack.append(num)
+            elif i == '[':
+                list.append(string) 
+                list.append(num)
                 string = ''
                 num = 0
-            elif c == ']':
-                pre_num = stack.pop()
-                pre_string = stack.pop()
-                string = pre_string + pre_num * string
+            
+            elif i == ']':
+                # a * 3 = aaa
+                num_list = list.pop()
+                string_list = list.pop()
+                string = string_list + num_list * string
+                
         return string
