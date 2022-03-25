9c78bfb29830b0ff3e8d57b61ada2d191be57525
Yuseung Kim <ykim49@ncsu.edu>
2022-03-25 11:45:33 -0400

    Time: 51 ms (29.77%), Space: 13.4 MB (89.88%) - LeetHub

---
+++ 162-find-peak-element/162-find-peak-element.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findPeakElement(self, nums):
+        left = 0
+        right = len(nums) - 1
+        
+        while left <= right - 1:
+            mid = (left + right) // 2
+            if nums[mid] > nums[mid + 1] and nums[mid] > nums[mid - 1]:
+                return mid
+            elif nums[mid] < nums[mid + 1]:
+                left = mid + 1
+            else:
+                right = mid - 1
+        if nums[left]>= nums[right]:
+            return left
+        else:
+            return right
