a587b47960945028ddb3629d112993dc61805409
Yuseung Kim <ykim49@ncsu.edu>
2022-03-25 20:59:14 -0400

    Time: 704 ms (59.51%), Space: 14.5 MB (45.51%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,9 +1,15 @@
+from collections import deque
+
 class Solution(object):
     def shortestPathBinaryMatrix(self, grid):
+        # 1 0 0
+        # 1 1 0
+        # 1 1 0
+        
         m, n = len(grid), len(grid[0])
         visited = set()
         q = deque()
-        dirs = [(0, 1), (0, -1), (1, 0), (-1, 0), (-1, -1), (1, 1), (-1, 1), (1, -1)]
+        direc = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, 1), (-1, -1), (1, -1)]
         
         if grid[0][0] == 0:
             q.append((1, (0, 0)))
@@ -11,13 +17,15 @@ class Solution(object):
             
         while q:
             steps, tmp = q.popleft()
-            r, c = tmp[0], tmp[1]
-            if (r, c) == (m-1, n-1):
+            l, k = tmp[0], tmp[1]
+            
+            if(l, k) == (m-1, n-1):
                 return steps
-            for (i,j) in dirs:
-                new_r,new_c = r+i, c+j
-                if 0 <= new_r < m and 0 <= new_c < n and grid[new_r][new_c] == 0 and (new_r, new_c) not in visited:
-                    q.append((steps+1, (new_r, new_c)))
-                    visited.add((new_r, new_c))
+            
+            for (i, j) in direc:
+                new_l, new_k = l+i, k+j
+                if 0 <= new_l < m and 0 <= new_k < n and grid[new_l][new_k] == 0 and (new_l, new_k) not in visited:
+                    q.append((steps + 1, (new_l, new_k)))
+                    visited.add((new_l, new_k))
                     
         return -1
