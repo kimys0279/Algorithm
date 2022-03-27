ea8bcab519c1c9b1fb6ff5ec978350fcc7e36b0d
Yuseung Kim <ykim49@ncsu.edu>
2022-03-27 16:49:23 -0400

    Time: 36 ms (56.63%), Space: 13.9 MB (13.27%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,6 +1,5 @@
 class Solution(object):
-   
-    def search(self, nums, target):
+    def search1(self, nums, target):
         if target not in nums:
             return -1
         
@@ -11,6 +10,7 @@ class Solution(object):
             
             if nums[mid] == target:
                 return mid
+            
             elif nums[low] <= nums[mid]:
                 if nums[low] <= target <= nums[mid]:
                     high = mid - 1
@@ -20,4 +20,63 @@ class Solution(object):
                 if nums[mid] <= target <= nums[high]:
                     low = mid + 1
                 else:
-                    high = mid - 1        
+                    high = mid - 1
+            
+            
+    def search(self, nums, target):
+        if not nums or target not in nums:
+            return -1
+        
+        l, r = 0, len(nums) - 1
+        
+        while l <= r:
+            mid = (l + r) // 2
+            
+            if nums[mid] == target:
+                return mid
+            
+            elif nums[l] <= nums[mid]:
+                if nums[l] <= target <= nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            
+            else:
+                if nums[mid] <= target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            
+            
+            
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
