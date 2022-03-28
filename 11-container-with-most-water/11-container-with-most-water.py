7bc2c885cfff81b45b4c4e517abf2ab9ac64156c
Yuseung Kim <ykim49@ncsu.edu>
2022-03-28 11:06:55 -0400

    Time: 600 ms (93.05%), Space: 24 MB (53.54%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -1,10 +1,5 @@
 class Solution(object):
-    def maxArea(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
-        
+    def maxArea1(self, height):
         left, right = 0, len(height) - 1
         max_height = len(height) - 1
         ans = 0
@@ -13,8 +8,21 @@ class Solution(object):
             if height[left] < height[right]:
                 ans = max(ans, i * height[left])
                 left += 1
-                
             else:
                 ans = max(ans, i * height[right])
                 right -= 1
+        return ans
+    
+    def maxArea(self, height):
+        l, r = 0, len(height) - 1
+        max_height = len(height) - 1
+        ans = 0
+        
+        for i in range(max_height, 0, -1):
+            if height[l] < height[r]:
+                ans = max(ans, i * height[l])
+                l += 1
+            else:
+                ans = max(ans, i * height[r])
+                r -= 1
         return ans
