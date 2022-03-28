dc73678512f13819ebc8742283258c3af3740262
Yuseung Kim <ykim49@ncsu.edu>
2022-03-28 10:41:43 -0400

    Time: 696 ms (72.94%), Space: 16.4 MB (91.68%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,14 +1,11 @@
 class Solution(object):
     def threeSum1(self, nums):
-
-        
         res = []
         nums.sort()
         
         for i in range(len(nums) - 2):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            
             l, r = i + 1, len(nums) - 1
             
             while l < r:
@@ -17,20 +14,16 @@ class Solution(object):
                     l += 1
                 elif s > 0:
                     r -= 1
-                    
                 else:
                     res.append((nums[i], nums[l], nums[r]))
                     
                     while l < r and nums[l] == nums[l + 1]:
                         l += 1
-                        
                     while l < r and nums[r] == nums[r - 1]:
                         r -= 1
                     l += 1
                     r -= 1
-                    
         return res
-    
     
     def threeSum(self, nums):
         res = []
@@ -54,6 +47,7 @@ class Solution(object):
                         l += 1
                     while l < r and nums[r] == nums[r - 1]:
                         r -= 1
+                        
                     l += 1
                     r -= 1
         return res
