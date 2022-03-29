d2f692a968520b8e0128ed77b4e8e8dfd74fe6e9
Yuseung Kim <ykim49@ncsu.edu>
2022-03-29 10:22:24 -0400

    Time: 32 ms (20.98%), Space: 13.6 MB (15.85%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -1,9 +1,5 @@
 class Solution(object):
-    def spiralOrder(self, matrix):
-        """
-        :type matrix: List[List[int]]
-        :rtype: List[int]
-        """
+    def spiralOrder1(self, matrix):
         left, right = 0, len(matrix[0]) - 1
         top, bottom = 0, len(matrix) - 1
 
@@ -38,4 +34,35 @@ class Solution(object):
             if left > right:
                 break
                     
+        return ans
+    
+    def spiralOrder(self, matrix):
+        left, right = 0, len(matrix[0]) - 1
+        top, bottom = 0, len(matrix) - 1
+        ans = []
+        
+        while True:
+            #top to right
+            ans.extend(matrix[top][x] for x in range(left, right + 1))
+            top += 1
+            if top > bottom:
+                break
+            
+            #Right go down
+            ans.extend(matrix[y][right] for y in range(top,bottom + 1))
+            right -= 1
+            if left > right:
+                break
+                
+            #Bottom go left
+            ans.extend(matrix[bottom][x] for x in range(right, left - 1, -1))
+            bottom -= 1
+            if top > bottom:
+                break
+                
+            #left go up
+            ans.extend(matrix[y][left] for y in range(bottom, top - 1, -1))
+            left += 1
+            if left > right:
+                break
         return ans
