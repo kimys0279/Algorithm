a09740e286c60dfa59be1f8c6f1b4f3de8ee8763
Yuseung Kim <ykim49@ncsu.edu>
2022-03-31 13:42:38 -0400

    Time: 186 ms (38.06%), Space: 14.8 MB (45.90%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,12 +1,15 @@
 class Solution(object):
-    def missingNumber(self, nums):
+    def missingNumber1(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
 
         big = len(nums)
-
         orisum = ((big + 1) * big) / 2
+        return orisum - sum(nums)
 
+    def missingNumber(self, nums):
+        big = len(nums)
+        orisum = ((big +1) * big) / 2
         return orisum - sum(nums)
