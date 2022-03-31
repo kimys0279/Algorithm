279579fe3145411ffb0baf12e90b51a99763e5e5
Yuseung Kim <ykim49@ncsu.edu>
2022-03-31 14:24:27 -0400

    Time: 174 ms (40.33%), Space: 18 MB (96.84%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -15,21 +15,15 @@ class Solution(object):
     
     
     
-    
-    
-    
-    
-    
     def merge(self, intervals):
         if len(intervals) == 0:
             return []
-        
-        intervals.sort(key = lambda x : x[0])
+        intervals.sort(key = lambda x: x[0])
         ans = []
         
         for i in intervals:
-            if len(ans) == 0 or i[0] > ans[-1][1]:
+            if (len(ans) == 0 or i[0] > ans[-1][1]):
                 ans.append(i)
-            elif ans[-1][1] >= i[0]:
+            elif (i[0] <= ans[-1][1]):
                 ans[-1][1] = max(ans[-1][1], i[1])
         return ans
