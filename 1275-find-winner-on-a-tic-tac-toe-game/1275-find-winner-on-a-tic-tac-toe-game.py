734a743459591e13c4279c2397abbf4169c9d9d7
Yuseung Kim <ykim49@ncsu.edu>
2022-04-01 21:18:41 -0400

    Time: 16 ms (94.81%), Space: 13.2 MB (91.00%) - LeetHub

--- 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.py
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.py
@@ -1,23 +1,30 @@
 class Solution(object):
     def tictactoe(self, moves):
-        n = 3
-        rows, cols = [0] * n, [0] * n
+        rows, cols = [0] * 3, [0] * 3
         d1, d2 = 0, 0
         player = 1
         
         for r, c in moves:
             rows[r] += player
             cols[c] += player
-            if r == c: d1 += player
-            if r+c == n-1: d2 += player
-            if abs(rows[r]) == n or abs(cols[c]) == n or abs(d1) == n or abs(d2) == n:
+            if r == c:
+                d1 += player
+            if r + c == 2:
+                d2 += player
+           
+            if abs(rows[r]) == 3 or abs(cols[c]) == 3 or abs(d1) == 3 or abs(d2) == 3:
                 if player == 1:
-                    return "A"
+                    return 'A'
                 else:
-                    return "B"
+                    return 'B'
             player *= -1
-        
-        if len(moves) == (n * n):
-            return "Draw"
+            
+        if len(moves) == 9:
+            return 'Draw'
         else:
-            return "Pending"
+            return 'Pending'
+        
+        
+        # row [0]
+        # one row or one col or any diag
+        # player A : 1 / player B : -1
