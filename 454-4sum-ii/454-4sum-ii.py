1275cf1b9045267c6f37efc0a813b09f857243ee
Yuseung Kim <ykim49@ncsu.edu>
2022-08-02 01:28:20 -0400

    Time: 580 ms (84.47%), Space: 13.8 MB (54.97%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.py
+++ 454-4sum-ii/454-4sum-ii.py
@@ -7,18 +7,19 @@ class Solution(object):
         :type nums4: List[int]
         :rtype: int
         """
-        hashtable = {}
-        for a in nums1:
-            for b in nums2:
-                if a+b in hashtable:
-                    hashtable[a+b] += 1
+        total_sum = 0
+        cache = {}
+        for i in nums3:
+            for j in nums4:
+                key = i + j
+                if key in cache:
+                    cache[key] += 1
                 else:
-                    hashtable[a+b] = 1
-        
-        count = 0
-        for c in nums3:
-            for d in nums4:
-                if -c-d in hashtable:
-                    count += hashtable[-c-d]
-        
-        return count
+                    cache[key] = 1
+        for i in nums1:
+            for j in nums2:
+                key = -i-j
+                if key in cache:
+                    total_sum += cache[key]
+                    
+        return total_sum
