2440126d92b415885b3bba6c17d734becf6976bf
Yuseung Kim <ykim49@ncsu.edu>
2022-08-19 18:57:48 -0400

    Time: 26 ms (90.12%), Space: 13.7 MB (36.65%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -5,6 +5,8 @@ class Solution(object):
         :rtype: List[int]
         """
         res = []
+        if len(nums) == 1:
+            return nums
         for i in range(1, len(nums) + 1):
             res.append(sum(nums[:i]))
         return res
