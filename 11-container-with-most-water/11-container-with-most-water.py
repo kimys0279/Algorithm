a6acadfb36c12c67ba8c7f7819cfce95cce32dc1
Yuseung Kim <ykim49@ncsu.edu>
2022-08-19 15:39:00 -0400

    Time: 734 ms (68.62%), Space: 24.1 MB (22.18%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -1,24 +1,9 @@
 class Solution(object):
-    def maxArea1(self, height):
-        left, right = 0, len(height) - 1
-        max_height = len(height) - 1
-        ans = 0
-        
-        for i in range(max_height, 0, -1):
-            if height[left] < height[right]:
-                ans = max(ans, i * height[left])
-                left += 1
-            else:
-                ans = max(ans, i * height[right])
-                right -= 1
-        return ans
-    
     def maxArea(self, height):
         l, r = 0, len(height) - 1
-        max_height = len(height) - 1
         ans = 0
         
-        for i in range(max_height, 0, -1):
+        for i in range(len(height) - 1, 0, -1):
             if height[l] < height[r]:
                 ans = max(ans, i * height[l])
                 l += 1
