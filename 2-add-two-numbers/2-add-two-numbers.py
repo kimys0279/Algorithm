3db0d10447a55a7fcd453bf787183bed72130d91
Yuseung Kim <ykim49@ncsu.edu>
2022-08-20 14:55:05 -0400

    Time: 87 ms (49.42%), Space: 13.5 MB (43.79%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -10,17 +10,21 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        dummyHead = ListNode(0)
-        curr = dummyHead
+        dummyhead = ListNode(0)
+        curr = dummyhead
         carry = 0
-        while l1 != None or l2 != None or carry != 0:
+        
+        while l1 or l2 or carry:
             l1Val = l1.val if l1 else 0
             l2Val = l2.val if l2 else 0
-            columnSum = l1Val + l2Val + carry
-            carry = columnSum // 10
-            newNode = ListNode(columnSum % 10)
+            
+            columnsum = l1Val + l2Val + carry
+            carry = columnsum // 10
+            
+            newNode = ListNode(columnsum % 10)
             curr.next = newNode
             curr = newNode
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
-        return dummyHead.next
+        
+        return dummyhead.next
