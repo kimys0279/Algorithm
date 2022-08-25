ad2dbf73203b7f0bbf99a3040fe1ae6f23b22fe7
Yuseung Kim <ykim49@ncsu.edu>
2022-08-25 10:18:50 -0400

    Time: 919 ms (42.10%), Space: 24.1 MB (22.18%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -5,13 +5,13 @@ class Solution(object):
         :rtype: int
         """
         l, r = 0, len(height) - 1
-        max_vol = 0
+        maxVol = 0
         
         for i in range(len(height)-1, 0, -1):
             if height[l] > height[r]:
-                max_vol = max(max_vol, i*height[r])
+                maxVol = max(maxVol, i*height[r])
                 r -= 1
             else:
-                max_vol = max(max_vol, i*height[l])
+                maxVol = max(maxVol, i*height[l])
                 l += 1
-        return max_vol
+        return maxVol
