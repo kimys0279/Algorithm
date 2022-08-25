1954f05bdde04130e79da354833f1da4ab2947f7
Yuseung Kim <ykim49@ncsu.edu>
2022-08-25 11:07:29 -0400

    Time: 112 ms (5.51%), Space: 13.6 MB (8.59%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        dic = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+        res = 0
+        i = 0
+        
+        while i < (len(s)):
+            if i+1 < len(s) and dic[s[i]] < dic[s[i+1]]:
+                res += dic[s[i+1]] - dic[s[i]]
+                i += 2
+            else:
+                res += (dic[s[i]])
+                i += 1
+        return res
+            
+        
