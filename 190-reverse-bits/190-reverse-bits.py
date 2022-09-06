6cee0f79dba797921ba9e424cafb61b162d976fd
Yuseung Kim <ykim49@ncsu.edu>
2022-09-05 22:35:33 -0400

    Time: 32 ms (95.50%), Space: 13.8 MB (94.08%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.py
+++ 190-reverse-bits/190-reverse-bits.py
@@ -1,9 +1,7 @@
 class Solution:
-    # @param n, an integer
-    # @return an integer
-    def reverseBits(self, n):
-        res = 0
-        for _ in range(32):
-            res = res*2 + (n & 1)
-            n = n >> 1
-        return res
+    def reverseBits(self, n: int) -> int:
+        n = bin(n)[2:]         # convert to binary, and remove the usual 0b prefix
+        n = '%32s' % n         # print number into a pre-formatted string with space-padding
+        n = n.replace(' ','0') # Convert the useful space-padding into zeros
+        # Now we have a  proper binary representation, so we can make the final transformation
+        return int(n[::-1],2)
