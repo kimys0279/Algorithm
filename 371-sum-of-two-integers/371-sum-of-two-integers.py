7b9b121e0c0643b2b51fec3998eb27c449d4dff8
Yuseung Kim <ykim49@ncsu.edu>
2022-09-05 21:40:29 -0400

    Time: 76 ms (5.52%), Space: 13.9 MB (14.89%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.py
+++ 371-sum-of-two-integers/371-sum-of-two-integers.py
@@ -1,8 +1,3 @@
-class Solution(object):
-    def getSum(self, a, b):
-        xor = a ^ b
-        carry = a & b
-        if carry == 0:
-            return xor
-        else:
-            return add(xor, carry << 1)
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        return add(a, b)
