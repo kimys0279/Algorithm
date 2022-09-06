f5115c0af4e4eb1a1b4dec470e992b6caf587b4e
Yuseung Kim <ykim49@ncsu.edu>
2022-09-05 22:06:18 -0400

    Time: 128 ms (65.37%), Space: 20.7 MB (97.33%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,20 +1,7 @@
-class Solution(object):
-    def countBits(self, n):
-        """
-        :type n: int
-        :rtype: List[int]
-        """
-        if n == 0:
-            return [0]
-        arr = [0] * (n + 1)
-        arr[0] = 0
-        arr[1] = 1
-        
-        for i in range(2, n + 1):
-            if i % 2 == 0:
-                arr[i] = arr[i/2]
-            else:
-                arr[i] = 1 + arr[(i - 1) / 2]
-                
-        return arr
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans = []
+        for i in range(n + 1):
+            ans.append(bin(i).count('1'))
+        return ans
         
