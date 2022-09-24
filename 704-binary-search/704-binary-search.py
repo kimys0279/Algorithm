6a9a871a1eda00ba5705606541457f5a7fdee0a7
Yuseung Kim <ykim49@ncsu.edu>
2022-09-24 19:30:11 -0400

    Time: 700 ms (5.05%), Space: 15.5 MB (26.68%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -1,10 +1,5 @@
-class Solution(object):
-    def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
         if target not in nums:
             return -1
         return nums.index(target)
