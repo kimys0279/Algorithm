989bd175df599771411dcb17c2f1144b255551b6
Yuseung Kim <ykim49@ncsu.edu>
2022-10-01 18:08:07 -0400

    Time: 56 ms (40.31%), Space: 13.9 MB (66.02%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -1,13 +1,9 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        triangle = []
+        pascal = [[1] * (i+1) for i in range(numRows)]
         
-        for row_num in range(numRows):
-            row = [None for _ in range(row_num + 1)]
-            row[0], row[-1] = 1, 1
-            
-            for j in range(1, len(row) - 1):
-                row[j] = triangle[row_num-1][j-1] + triangle[row_num-1][j]
-            triangle.append(row)
-            
-        return triangle
+        for i in range(numRows):
+            for j in range(1, i):
+                pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j]
+                
+        return pascal
