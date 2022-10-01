4117a7be6032822b9ca9d48fbbc808940bf538db
Yuseung Kim <ykim49@ncsu.edu>
2022-10-01 18:04:44 -0400

    Time: 32 ms (93.55%), Space: 13.8 MB (66.02%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -7,7 +7,7 @@ class Solution:
             row[0], row[-1] = 1, 1
             
             for j in range(1, len(row) - 1):
-                row[j] = triangle[row_num - 1][j-1] + triangle[row_num - 1][j]
-            
+                row[j] = triangle[row_num-1][j-1] + triangle[row_num-1][j]
             triangle.append(row)
+            
         return triangle
