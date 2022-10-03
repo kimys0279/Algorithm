71903b827d4d31d6553dd43085d77e6e15452a08
Yuseung Kim <ykim49@ncsu.edu>
2022-10-03 10:59:39 -0400

    Time: 112 ms (35.09%), Space: 14.1 MB (52.96%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.py
@@ -0,0 +1,7 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for i in ransomNote:
+            if i not in magazine:
+                return False
+            magazine = magazine[:magazine.index(i)] + magazine[magazine.index(i)+1:]
+        return True
