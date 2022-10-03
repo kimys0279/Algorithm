4811781a743ac8961cb319b59ed9499202d210d5
Yuseung Kim <ykim49@ncsu.edu>
2022-10-03 11:07:25 -0400

    Time: 1018 ms (5.01%), Space: 14.5 MB (34.85%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -2,18 +2,8 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        
-        dic_s = {}
-        dic_t = {}
-        
         for i in s:
-            if i not in dic_s:
-                dic_s[i] = 1
-            dic_s[i] += 1
-        
-        for i in t:
-            if i not in dic_t:
-                dic_t[i] = 1
-            dic_t[i] += 1
-        
-        return dic_s == dic_t
+            if i not in t:
+                return False
+            t = t[:t.index(i)] + t[t.index(i)+1:]
+        return True
