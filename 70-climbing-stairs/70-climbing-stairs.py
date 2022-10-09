066cf0f71ab2718a047f0664d89eb5b43014a6af
Yuseung Kim <ykim49@ncsu.edu>
2022-10-09 15:44:09 -0400

    Time: 28 ms (97.05%), Space: 13.9 MB (56.94%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,14 +1,8 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        li = [2, 3]
+        nums = [1, 2]
         
-        if n == 1:
-            return 1
-        
-        if n == 2:
-            return 2
-        
-        for i in range(2, n - 1):
-            li.append(li[i-2] + li[i-1])
-        
-        return li.pop()
+        for i in range(2, n+1):
+            nums.append(nums[i-1]+nums[i-2])
+            
+        return nums[n-1]
