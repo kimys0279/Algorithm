b26b546a90331182fc4f996dd13f828b2920e642
Yuseung Kim <ykim49@ncsu.edu>
2022-10-09 09:48:43 -0400

    Time: 118 ms (9.96%), Space: 14.7 MB (82.51%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -1,6 +1,9 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         if target not in nums:
-            nums.append(target)
-            return sorted(nums).index(target)
+            if target > nums[-1]:
+                return nums.index(nums[-1]) + 1
+            for i in nums:
+                if i > target:
+                    return nums.index(i)
         return nums.index(target)
